d2267dc2034942789513a59c671a5bf43e4c2a3d
Sue-cod <snozic892@gmail.com>
2021-06-11 15:23:47 +0200

    Video browser app

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,16 +1,3 @@
-/*import axios from 'axios';
-const KEY = 'AIzaSyDv9oEXK7ZMQY9e0n0sEs4FGwYkkeC4jtA';
-
-export default axios.create({
-    baseURL: 'http://www.googleapis.com/youtube/v3/',
-              
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY,
-    },
-});*/
-
 import axios from "axios";
 
 const KEY = "AIzaSyDv9oEXK7ZMQY9e0n0sEs4FGwYkkeC4jtA";
